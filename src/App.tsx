bcd27082b6d00edccacde66e681e26f8feda21e7
tompra <tomprayon@gmail.com>
2024-03-20 18:03:37 +0100

    handle input change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-function App() {
+import { useState, ChangeEvent } from 'react';
+
+const App = () => {
+    const [userInput, setUserInput] = useState<string>('');
+
+    const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const searchInput = e.target.value;
+        if (searchInput === '') return;
+        setUserInput(searchInput);
+    };
+
     return (
         <main className='flex justify-center items-center bg-sky-500 h-[100vh]'>
             <section className='bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded w-full md:max-w-[500px] flex flex-col text-center items-center p-4 justify-center md:px-10 lg:p-24 h-full lg:h-[500px]'>
@@ -9,10 +19,11 @@ function App() {
                     Enter a place you want to know the weather and select an
                     option
                 </p>
-                <div className='flex mt-10 md:mt-4'>
+                <div className='flex mt-10 md:mt-4 relative'>
                     <input
                         type='text'
-                        value={'k'}
+                        value={userInput}
+                        onChange={inputChange}
                         className='px-2 py-2 border-2 border-white rounded-l-md '
                     />
 
@@ -23,6 +34,5 @@ function App() {
             </section>
         </main>
     );
-}
-
+};
 export default App;
