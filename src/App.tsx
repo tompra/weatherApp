42560f99854b4c0ef59ed1a2059c56f552333da5
tompra <tomprayon@gmail.com>
2024-03-20 18:33:15 +0100

    add city state and handle selected option

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState, ChangeEvent } from 'react';
 import { REACT_APP_API_KEY } from '../secret.json';
+import { OptionType } from './types';
 
-const App = () => {
+const App: React.FC = (): JSX.Element => {
     const [userInput, setUserInput] = useState<string>('');
     const [options, setOptions] = useState<[]>([]);
+    const [city, setCity] = useState<OptionType | null>(null);
 
     const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
         const searchInput = e.target.value.trim();
@@ -27,6 +29,11 @@ const App = () => {
         }
     };
 
+    const onOptionSelect = async (option: OptionType) => {
+        setCity(option);
+        setOptions([]);
+    };
+
     return (
         <main className='flex justify-center items-center bg-sky-500 h-[100vh]'>
             <section className='bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded w-full md:max-w-[500px] flex flex-col text-center items-center p-4 justify-center md:px-10 lg:p-24 h-full lg:h-[500px]'>
@@ -45,11 +52,14 @@ const App = () => {
                         className='px-2 py-2 border-2 border-white rounded-l-md '
                     />
                     <ul className='absolute top-11 bg-white ml-1 rounded-b-md'>
-                        {options.map((option, index) => {
+                        {options.map((option: OptionType, index) => {
                             const { name, country } = option;
                             return (
                                 <li key={`${name}-${index}`}>
-                                    <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'>
+                                    <button
+                                        className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'
+                                        onClick={() => onOptionSelect(option)}
+                                    >
                                         {name}, {country}
                                     </button>
                                 </li>
