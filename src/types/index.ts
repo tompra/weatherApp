c88a18df37f321901c1e0726bf572b7b38a9c1ea
tompra <tomprayon@gmail.com>
2024-03-20 19:18:59 +0100

    create forecast type

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,39 @@ export type OptionType = {
     lat: number;
     lon: number;
 };
+
+export type ForecastType = {
+    name: string;
+    country: string;
+    sunrise: number;
+    sunset: number;
+    list: [
+        {
+            dt: number;
+            main: {
+                feels_like: number;
+                humidity: number;
+                pressure: number;
+                temp: number;
+                temp_max: number;
+                temp_min: number;
+            };
+            weather: [
+                {
+                    main: string;
+                    description: string;
+                    icon: string;
+                }
+            ];
+            wind: {
+                speed: number;
+                deg: number;
+                gust: number;
+            };
+            clouds: {
+                all: number;
+            };
+            visibility: number;
+        }
+    ];
+};
