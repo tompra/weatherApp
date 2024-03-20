00043efc5bd542b383bc3ffdf4980375a5dbc9e2
tompra <tomprayon@gmail.com>
2024-03-20 19:03:28 +0100

    use fetch data hook

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,2 +1,62 @@
-const useFetchData = () => {};
+import { useState, ChangeEvent } from 'react';
+import { REACT_APP_API_KEY } from '../../secret.json';
+import { OptionType } from '../types/index';
+
+const useFetchData = () => {
+    const [userInput, setUserInput] = useState<string>('');
+    const [options, setOptions] = useState<[]>([]);
+    const [city, setCity] = useState<OptionType | null>(null);
+
+    const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const searchInput = e.target.value;
+        if (searchInput === '') return;
+        setUserInput(searchInput);
+        getSearch(searchInput);
+    };
+
+    const getSearch = async (value: string) => {
+        try {
+            const response = await fetch(
+                `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${REACT_APP_API_KEY}`
+            );
+            if (!response.ok) {
+                console.error(`Error the fetch data is not OK`);
+            }
+            const data = await response.json();
+            setOptions(data);
+        } catch (error) {
+            console.log(`Error happened by fetching data: ${error}`);
+        }
+    };
+
+    const onSubmit = () => {
+        if (!city) return;
+        getForecast(city);
+    };
+
+    const getForecast = async (city: OptionType) => {
+        const { lat, lon } = city;
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${REACT_APP_API_KEY}`
+            );
+            if (!response.ok) {
+                console.error(`Error the fetch data is not OK`);
+            }
+            const data = await response.json();
+            console.log('data', data);
+            setCity(data);
+            // setUserInput(data.name);
+            setUserInput('');
+        } catch (error) {
+            console.log(`Error happened by fetching data: ${error}`);
+        }
+    };
+
+    const onOptionSelect = (option: OptionType) => {
+        setCity(option);
+        setOptions([]);
+    };
+    return { userInput, inputChange, options, onOptionSelect, onSubmit };
+};
 export default useFetchData;
