4f10f6621b1da98ef238cd8a65d1bcb6047ab81b
tompra <tomprayon@gmail.com>
2024-03-21 11:37:54 +0100

    render wind component

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,8 +1,9 @@
-import { ForecastType } from '../types';
 import HeaderForecast from './HeaderForecast';
 import ImagesForecast from './ImagesForecast';
 import InfoSuntime from './InfoSuntime';
-import { findIcon } from '../utils/helpers';
+import WeatherInfo from './WeatherInfo';
+import { ForecastType } from '../types';
+import { findIcon, getWindDirection } from '../utils/helpers';
 
 type Props = {
     forecast: ForecastType | null;
@@ -12,6 +13,15 @@ const Forecast: React.FC<Props> = ({ forecast }): JSX.Element => {
     if (!forecast) {
         return <div>Loading...</div>;
     }
+
+    const today = forecast.list[0];
+
+    const wind = {
+        description: `${getWindDirection(
+            Math.round(today.wind.deg)
+        )}, gusts: ${today.wind.gust.toFixed(1)} km/h`,
+        info: `${Math.round(today.wind.speed)} km/h`,
+    };
 
     return (
         <section className='w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24  h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg'>
@@ -25,6 +35,12 @@ const Forecast: React.FC<Props> = ({ forecast }): JSX.Element => {
                 <InfoSuntime
                     title={findIcon('sunset')}
                     value={forecast.sunset}
+                />
+                <WeatherInfo
+                    icon={findIcon('wind')}
+                    title='wind'
+                    info={wind.info}
+                    description={wind.description}
                 />
             </div>
         </section>
