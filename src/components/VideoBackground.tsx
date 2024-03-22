03d98e0469da28f13592d12f73299111da31719c
tompra <tomprayon@gmail.com>
2024-03-22 12:08:13 +0100

    Revert "create ref for watching changes for weather"
    This reverts commit d507f1550faa7223e541f33fc2e87fe342a48caa.

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,33 +1,18 @@
 import { getBackgroundVideo } from '../utils/helpers';
-import { useEffect, useRef } from 'react';
 
 type Props = {
     video: string;
 };
 
 const VideoBackground: React.FC<Props> = ({ video }): JSX.Element => {
-    const videoRef = useRef<HTMLVideoElement>(null);
-
-    useEffect(() => {
-        const videoElement = videoRef.current;
-        if (videoElement) {
-            const videoSource = getBackgroundVideo(video);
-            if (videoSource !== 'none') {
-                videoElement.src = videoSource;
-                videoElement.load();
-            }
-        }
-    }, [video]);
-
     return (
         <video
-            ref={videoRef}
             loop
             muted
             autoPlay
             className='absolute inset-0 w-full h-full object-cover -z-10'
         >
-            <source src='' type='video/mp4' />
+            <source src={`${getBackgroundVideo(video)}`} type='video/mp4' />
             Your browser does not support the video tag.
         </video>
     );
