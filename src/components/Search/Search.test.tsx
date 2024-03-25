81ed9e94436269475d33eaff8bd8de84f7a1e15f
tompra <tomprayon@gmail.com>
2024-03-25 19:46:58 +0100

    add new test case

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { vi, describe, test, expect } from 'vitest';
 import Search from './Search';
-import * as useFetchData from '../../hooks/useFetchData';
 
 describe('Testing search component', () => {
+    const inputChangeMock = vi.fn();
     test('search component render initially', () => {
         const wrapper = render(
             <Search
@@ -15,5 +15,20 @@ describe('Testing search component', () => {
             />
         );
         expect(wrapper).toBeTruthy();
+        const searchComponent = screen.getByTestId('search-component');
+        expect(searchComponent).toBeInTheDocument();
+    });
+    test('user fire the input field by change', async () => {
+        render(
+            <Search
+                userInput=''
+                inputChange={inputChangeMock}
+                options={[]}
+                onSubmit={vi.fn()}
+                onOptionSelect={vi.fn()}
+            />
+        );
+        const input = screen.getByRole('textbox') as HTMLInputElement;
+        expect(input).toBeInTheDocument();
     });
 });
