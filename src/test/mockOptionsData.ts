172d17742726e2d999d6cc722f07cee75071eac6
tompra <tomprayon@gmail.com>
2024-03-25 23:15:01 +0100

    mock options data retrieve

---
+++ src/test/mockOptionsData.ts
@@ -0,0 +1,34 @@
+import { OptionType } from '../types';
+
+export const mockOptionsData: OptionType[] = [
+    {
+        name: 'Linz',
+        country: 'AT',
+        lat: 48.3059078,
+        lon: 14.286198,
+    },
+    {
+        name: 'Li',
+        country: 'TH',
+        lat: 17.8070809,
+        lon: 98.9554659,
+    },
+    {
+        name: 'Lakes',
+        country: 'GB',
+        lat: 54.441232049999996,
+        lon: -3.0251145192001667,
+    },
+    {
+        name: 'Litle Reksøya',
+        country: 'NO',
+        lat: 67.03896395000001,
+        lon: 13.60456877210959,
+    },
+    {
+        name: 'Littj-Steinsøya',
+        country: 'NO',
+        lat: 67.04338680000001,
+        lon: 13.654644635511815,
+    },
+];
