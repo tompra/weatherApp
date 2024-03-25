46fe8c22f43e919e88d36297b97dbf6cfe2ee9ae
tompra <tomprayon@gmail.com>
2024-03-25 19:46:04 +0100

    mock HTMLMedia element instance to prevent error thrown

--- src/test/setupTest.ts
+++ src/test/setupTest.ts
@@ -1,2 +1,11 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/react';
+
+afterEach(() => {
+    cleanup();
+});
+
+Object.defineProperty(HTMLMediaElement.prototype, 'load', {
+    value: () => {},
+});
