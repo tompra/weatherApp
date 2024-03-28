383273bf5e66c9553fbdfd5f540a21106e0eb334
tompra <tomprayon@gmail.com>
2024-03-28 21:43:11 +0100

    add data-testid attribute for testing

--- src/components/InfoSuntime.tsx
+++ src/components/InfoSuntime.tsx
@@ -14,7 +14,10 @@ const getSunTime = (time: number): string => {
 
 const InfoSuntime: React.FC<Props> = ({ icon, value, title }): JSX.Element => {
     return (
-        <div className='w-full md:w-[45%] h-[120px] text-xs md:text-sm font-bold flex flex-col justify-center items-center bg-white/20 backdrop-blur-lg rounded drop-shadow-lg py-4 mb-2 hover:bg-white/40 hover:backdrop-blur-lg hover:drop-shadow-lg hover:rounded'>
+        <div
+            className='w-full md:w-[45%] h-[120px] text-xs md:text-sm font-bold flex flex-col justify-center items-center bg-white/20 backdrop-blur-lg rounded drop-shadow-lg py-4 mb-2 hover:bg-white/40 hover:backdrop-blur-lg hover:drop-shadow-lg hover:rounded'
+            data-testid='info-suntime'
+        >
             {icon}
             <span>{getSunTime(value)}</span>
             <p className='capitalize mt-1'>{title}</p>
