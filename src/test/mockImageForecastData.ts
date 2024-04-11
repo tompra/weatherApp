d65d8b045ec7b6837963553194b2a2cb3cdccb60
tompra <tomprayon@gmail.com>
2024-04-11 15:20:07 +0200

    create a mock forecast data

---
+++ src/test/mockImageForecastData.ts
@@ -0,0 +1,609 @@
+export const imagesForecastDataMock = {
+    id: 470546,
+    name: 'Vyborg',
+    coord: {
+        lat: 60.7547,
+        lon: 28.6486,
+    },
+    country: 'RU',
+    population: 78633,
+    timezone: 10800,
+    sunrise: 1712804248,
+    sunset: 1712855690,
+    list: [
+        {
+            dt: 1712847600,
+            main: {
+                temp: 9.68,
+                feels_like: 6.53,
+                temp_min: 5.98,
+                temp_max: 9.68,
+                pressure: 1014,
+                sea_level: 1014,
+                grnd_level: 1011,
+                humidity: 68,
+                temp_kf: 3.7,
+            },
+            weather: [
+                {
+                    id: 802,
+                    main: 'Clouds',
+                    description: 'scattered clouds',
+                    icon: '03d',
+                },
+            ],
+            clouds: {
+                all: 47,
+            },
+            wind: {
+                speed: 6.98,
+                deg: 235,
+                gust: 15.63,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-11 15:00:00',
+        },
+        {
+            dt: 1712858400,
+            main: {
+                temp: 6.62,
+                feels_like: 3.38,
+                temp_min: 4.16,
+                temp_max: 6.62,
+                pressure: 1012,
+                sea_level: 1012,
+                grnd_level: 1008,
+                humidity: 81,
+                temp_kf: 2.46,
+            },
+            weather: [
+                {
+                    id: 500,
+                    main: 'Rain',
+                    description: 'light rain',
+                    icon: '10n',
+                },
+            ],
+            clouds: {
+                all: 73,
+            },
+            wind: {
+                speed: 4.96,
+                deg: 220,
+                gust: 13.01,
+            },
+            visibility: 10000,
+            pop: 1,
+            rain: {
+                '3h': 1.14,
+            },
+            sys: {
+                pod: 'n',
+            },
+            dt_txt: '2024-04-11 18:00:00',
+        },
+        {
+            dt: 1712869200,
+            main: {
+                temp: 4.32,
+                feels_like: 0.31,
+                temp_min: 4.32,
+                temp_max: 4.32,
+                pressure: 1007,
+                sea_level: 1007,
+                grnd_level: 1005,
+                humidity: 94,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 500,
+                    main: 'Rain',
+                    description: 'light rain',
+                    icon: '10n',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 5.36,
+                deg: 224,
+                gust: 14.62,
+            },
+            visibility: 10000,
+            pop: 1,
+            rain: {
+                '3h': 2.66,
+            },
+            sys: {
+                pod: 'n',
+            },
+            dt_txt: '2024-04-11 21:00:00',
+        },
+        {
+            dt: 1712880000,
+            main: {
+                temp: 4.12,
+                feels_like: 0.47,
+                temp_min: 4.12,
+                temp_max: 4.12,
+                pressure: 1005,
+                sea_level: 1005,
+                grnd_level: 1003,
+                humidity: 94,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 500,
+                    main: 'Rain',
+                    description: 'light rain',
+                    icon: '10n',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 4.54,
+                deg: 238,
+                gust: 11.01,
+            },
+            visibility: 8305,
+            pop: 1,
+            rain: {
+                '3h': 2.27,
+            },
+            sys: {
+                pod: 'n',
+            },
+            dt_txt: '2024-04-12 00:00:00',
+        },
+        {
+            dt: 1712890800,
+            main: {
+                temp: 4.87,
+                feels_like: 1.74,
+                temp_min: 4.87,
+                temp_max: 4.87,
+                pressure: 1005,
+                sea_level: 1005,
+                grnd_level: 1003,
+                humidity: 96,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 500,
+                    main: 'Rain',
+                    description: 'light rain',
+                    icon: '10d',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 3.93,
+                deg: 246,
+                gust: 11.48,
+            },
+            visibility: 8835,
+            pop: 1,
+            rain: {
+                '3h': 0.87,
+            },
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-12 03:00:00',
+        },
+        {
+            dt: 1712901600,
+            main: {
+                temp: 5.42,
+                feels_like: 1.91,
+                temp_min: 5.42,
+                temp_max: 5.42,
+                pressure: 1008,
+                sea_level: 1008,
+                grnd_level: 1005,
+                humidity: 85,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 500,
+                    main: 'Rain',
+                    description: 'light rain',
+                    icon: '10d',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 4.89,
+                deg: 282,
+                gust: 9.13,
+            },
+            visibility: 10000,
+            pop: 1,
+            rain: {
+                '3h': 0.14,
+            },
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-12 06:00:00',
+        },
+        {
+            dt: 1712912400,
+            main: {
+                temp: 5.22,
+                feels_like: 2.06,
+                temp_min: 5.22,
+                temp_max: 5.22,
+                pressure: 1010,
+                sea_level: 1010,
+                grnd_level: 1007,
+                humidity: 84,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04d',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 4.12,
+                deg: 295,
+                gust: 7.54,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-12 09:00:00',
+        },
+        {
+            dt: 1712923200,
+            main: {
+                temp: 7.4,
+                feels_like: 4.78,
+                temp_min: 7.4,
+                temp_max: 7.4,
+                pressure: 1012,
+                sea_level: 1012,
+                grnd_level: 1010,
+                humidity: 70,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 500,
+                    main: 'Rain',
+                    description: 'light rain',
+                    icon: '10d',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 4.05,
+                deg: 308,
+                gust: 6.36,
+            },
+            visibility: 10000,
+            pop: 0.2,
+            rain: {
+                '3h': 0.1,
+            },
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-12 12:00:00',
+        },
+        {
+            dt: 1712934000,
+            main: {
+                temp: 7.48,
+                feels_like: 5.75,
+                temp_min: 7.48,
+                temp_max: 7.48,
+                pressure: 1013,
+                sea_level: 1013,
+                grnd_level: 1010,
+                humidity: 67,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04d',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 2.62,
+                deg: 305,
+                gust: 4.82,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-12 15:00:00',
+        },
+        {
+            dt: 1712944800,
+            main: {
+                temp: 5.27,
+                feels_like: 5.27,
+                temp_min: 5.27,
+                temp_max: 5.27,
+                pressure: 1014,
+                sea_level: 1014,
+                grnd_level: 1011,
+                humidity: 83,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04n',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 1.13,
+                deg: 125,
+                gust: 1.03,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'n',
+            },
+            dt_txt: '2024-04-12 18:00:00',
+        },
+        {
+            dt: 1712955600,
+            main: {
+                temp: 3.52,
+                feels_like: 3.52,
+                temp_min: 3.52,
+                temp_max: 3.52,
+                pressure: 1014,
+                sea_level: 1014,
+                grnd_level: 1011,
+                humidity: 88,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04n',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 0.49,
+                deg: 137,
+                gust: 0.38,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'n',
+            },
+            dt_txt: '2024-04-12 21:00:00',
+        },
+        {
+            dt: 1712966400,
+            main: {
+                temp: 2.59,
+                feels_like: 2.59,
+                temp_min: 2.59,
+                temp_max: 2.59,
+                pressure: 1014,
+                sea_level: 1014,
+                grnd_level: 1011,
+                humidity: 91,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04n',
+                },
+            ],
+            clouds: {
+                all: 97,
+            },
+            wind: {
+                speed: 1.29,
+                deg: 107,
+                gust: 1.21,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'n',
+            },
+            dt_txt: '2024-04-13 00:00:00',
+        },
+        {
+            dt: 1712977200,
+            main: {
+                temp: 1.78,
+                feels_like: 0.17,
+                temp_min: 1.78,
+                temp_max: 1.78,
+                pressure: 1014,
+                sea_level: 1014,
+                grnd_level: 1011,
+                humidity: 94,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04d',
+                },
+            ],
+            clouds: {
+                all: 95,
+            },
+            wind: {
+                speed: 1.57,
+                deg: 117,
+                gust: 1.58,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-13 03:00:00',
+        },
+        {
+            dt: 1712988000,
+            main: {
+                temp: 4.86,
+                feels_like: 3.48,
+                temp_min: 4.86,
+                temp_max: 4.86,
+                pressure: 1014,
+                sea_level: 1014,
+                grnd_level: 1011,
+                humidity: 83,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04d',
+                },
+            ],
+            clouds: {
+                all: 97,
+            },
+            wind: {
+                speed: 1.75,
+                deg: 149,
+                gust: 2.88,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-13 06:00:00',
+        },
+        {
+            dt: 1712998800,
+            main: {
+                temp: 7.14,
+                feels_like: 5.47,
+                temp_min: 7.14,
+                temp_max: 7.14,
+                pressure: 1014,
+                sea_level: 1014,
+                grnd_level: 1011,
+                humidity: 72,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04d',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 2.46,
+                deg: 201,
+                gust: 3.26,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-13 09:00:00',
+        },
+        {
+            dt: 1713009600,
+            main: {
+                temp: 7.28,
+                feels_like: 5.21,
+                temp_min: 7.28,
+                temp_max: 7.28,
+                pressure: 1013,
+                sea_level: 1013,
+                grnd_level: 1010,
+                humidity: 70,
+                temp_kf: 0,
+            },
+            weather: [
+                {
+                    id: 804,
+                    main: 'Clouds',
+                    description: 'overcast clouds',
+                    icon: '04d',
+                },
+            ],
+            clouds: {
+                all: 100,
+            },
+            wind: {
+                speed: 3.06,
+                deg: 202,
+                gust: 4.65,
+            },
+            visibility: 10000,
+            pop: 0,
+            sys: {
+                pod: 'd',
+            },
+            dt_txt: '2024-04-13 12:00:00',
+        },
+    ],
+};
