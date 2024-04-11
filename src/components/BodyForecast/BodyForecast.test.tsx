8ffcdd99c11226f76c9cac33b54de5ed71775433
tompra <tomprayon@gmail.com>
2024-04-11 13:23:53 +0200

    perform test case for body forecast component

--- src/components/BodyForecast/BodyForecast.test.tsx
+++ src/components/BodyForecast/BodyForecast.test.tsx
@@ -10,5 +10,51 @@ describe('Test for body forecast component', () => {
 
     test('renders correctly with forecast data', () => {
         render(<BodyForecast forecast={mockForecastData} />);
+
+        const infoSuntime = screen.getAllByTestId('info-suntime');
+
+        infoSuntime.forEach((element) => {
+            expect(element).toBeInTheDocument();
+        });
+
+        const weatherInfo = screen.getAllByTestId('weather-info');
+        expect(weatherInfo).toHaveLength(6);
+
+        weatherInfo.forEach((element) => {
+            expect(element).toBeInTheDocument();
+        });
+    });
+
+    test('displays correct weather information', () => {
+        render(<BodyForecast forecast={mockForecastData} />);
+
+        const wind = screen.getByText('wind');
+        const realFeel = screen.getByText('real feel');
+        const precipitation = screen.getByText('precipitation');
+        const humidity = screen.getByText('humidity');
+        const pressure = screen.getByText('pressure');
+        const visibility = screen.getByText('visibility');
+
+        expect(wind).toBeInTheDocument();
+        expect(realFeel).toBeInTheDocument();
+        expect(precipitation).toBeInTheDocument();
+        expect(humidity).toBeInTheDocument();
+        expect(pressure).toBeInTheDocument();
+        expect(visibility).toBeInTheDocument();
+    });
+
+    test('pass correct props to Weather Info component', () => {
+        render(<BodyForecast forecast={mockForecastData} />);
+
+        const weatherInfo = screen.getAllByTestId('weather-info');
+        expect(weatherInfo).toHaveLength(6);
+
+        weatherInfo.forEach((component) => {
+            const childrenArray = Array.from(component.children);
+            const childrenContent = childrenArray.map(
+                (child) => child.innerHTML
+            );
+            expect(childrenContent).toHaveLength(3);
+        });
     });
 });
