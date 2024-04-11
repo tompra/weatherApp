0187ee7e97f90505d8f400187450fa1ad6d1f3d0
tompra <tomprayon@gmail.com>
2024-04-11 16:51:02 +0200

    create test case for component

--- src/components/Temperature/Temperature.test.tsx
+++ src/components/Temperature/Temperature.test.tsx
@@ -1,3 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
 import Temperature from './Temperature';
+
+describe('Test the temperature component', () => {
+    test('render the component without an error', () => {
+        render(<Temperature temp={10} />);
+    });
+    test('render the component the temperature text', () => {
+        render(<Temperature temp={10} />);
+
+        const temperature = screen.getByTestId('temperature-component');
+
+        expect(temperature.textContent).toBe('10oC');
+    });
+});
