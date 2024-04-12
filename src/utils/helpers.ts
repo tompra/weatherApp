f3705f799e909a08aa1df97c5ad66162f267a8e9
tompra <tomprayon@gmail.com>
2024-04-12 10:38:41 +0200

    add get sun time helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -119,3 +119,11 @@ export const getVisibilityValue = (number: number): string => {
             return 'Very clear day';
     }
 };
+
+export const getSunTime = (time: number): string => {
+    const date = new Date(time * 1000);
+    const hours = date.getHours().toString();
+    const minutes = date.getMinutes().toString();
+
+    return `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}`;
+};
